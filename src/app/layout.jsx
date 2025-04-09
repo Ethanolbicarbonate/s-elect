04a42e04c5cdc0329f4845bf9901958e85232fa3
Ethan Jed Carbonell <ethanjedllenobrac@gmail.com>
2025-04-10 01:41:48 +0800

    added icon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./global.css";
 import 'bootstrap/dist/css/bootstrap.css';
+import "./global.css";
 import BootstrapClient from '@/components/BootstrapClient.js';
 
 const geistSans = Geist({
@@ -21,6 +21,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="apple-mobile-web-app-title" content="sELECT" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} logo-font`}>
         {children}
         <BootstrapClient />
--- src/components/RoleSelection.js
+++ src/components/RoleSelection.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function RoleSelection() {
   return (
     <div className="flex-grow-1 d-flex flex-column align-items-center p-2">
-      <div className="d-flex align-items-center w-75 h-50 custom-width-sm-min">
+      <div className="d-flex align-items-center w-75 custom-width-sm-min">
         <div className="d-flex flex-column gap-2 w-100">
           <Link href="/user-login" className="btn custom-btn fs-5 btn-lg text-secondary border shadow-sm">
             User
