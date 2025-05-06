6bd710c7ddc880288c01703ac3f6d7aab09876a8
Ethan Jed Carbonell <131255754+Ethanolbicarbonate@users.noreply.github.com>
2025-05-06 20:59:36 +0800

    debugging

--- src/app/(student)/candidates/[candidateId]/loading.js
+++
@@ -1,3 +0,0 @@
-export default function LoadingCandidateDetail() {
-    return <p>Loading candidate details...</p>;
-  }
--- src/app/(student)/candidates/page.js
+++
@@ -1,3 +0,0 @@
-export default function candidates() {
-    return <div>Coming soon...</div>;
-  }
