dc8df2b51d358600a52dbe0d43a17008d40ba00f
Ethan Jed Carbonell <131255754+Ethanolbicarbonate@users.noreply.github.com>
2025-05-10 15:53:45 +0800

    fixed case sensitivity in login

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,3 @@
-// src/app/api/auth/[...nextauth]/route.js
-      
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
